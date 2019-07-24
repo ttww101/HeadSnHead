e3220712964e3bc0528a4f05fd652ef3708ad411
amG <bomiishere@gmail.com>
2019-07-24 20:34:39 +0800

    Initial Commit

---
+++ ShopSide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopSide
+//
+//  Created by Wu on 2019/7/24.
+//  Copyright © 2019 FCCutomer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
